7ef9a2f1be7fbdb04669d9b62ffc99e2778ec383
Jessica <Jessica@10.0.2.52>
2017-05-06 22:39:07 -0300

    Merge origin/master
    Conflicts:
    	.gitignore
    	Leilao/src/leilao/ReadingThread.java
    	Leilao/src/leilao/UniCastServer.java

---
+++ Server/src/server/ServImpl.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author allan
+ */
+public class ServImpl extends UnicastRemoteObject  implements InterfaceServ{
+
+    public ServImpl() throws RemoteException {
+    }
+
+
+    @Override
+    public void chamar(String nomeCliente, InterfaceCli refCliente) throws RemoteException{
+        refCliente.echo(nomeCliente + "acabou de chamar o servidor");
+       
+    }
+   
+}
---
+++ Server/src/server/Servidor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author Jessica
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.rmi.RemoteException
+     */
+    public static void main(String[] args) throws RemoteException {
+        
+       toInitializeNameService();
+        
+        
+        
+    }
+    
+    /** Method to initialize the Name Service
+     *   @return */
+    
+    public static void toInitializeNameService() throws RemoteException{
+        ServImpl server = new ServImpl();
+        int port = 6789;
+        Registry referenciaServicoNomes = LocateRegistry.createRegistry(port);
+        referenciaServicoNomes.rebind("Hello World", new ServImpl());
+        System.out.println("Server initialize");
+    }
+    
+}
